228e161ba21034c08428bcf3b4070a9acd2f2bd4
Javier <jeponteg@gmail.com>
2021-03-21 17:47:35 -0300

    Otros archivos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PopularMovieContex from "./contexts/PopularMovieContext";
+import VideoContex from "./contexts/VideoContext";
+import DetailsContext from "./contexts/DetailsCardContext";
+import Layout from "./components/layouts/Layout";
+import HomeScreen from "./screens/HomeScreen/HomeScreen";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import DetailsCard from "./components/Details-card/DetailsCard";
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      
+      <PopularMovieContex>
+        <VideoContex>
+          <Layout>
+            <Switch>
+              <Route exact path='/'>
+                <HomeScreen/>
+              </Route>
+
+              <DetailsContext>
+                <Route exact path="/movie-details/:id">
+                  <DetailsCard/>
+                </Route>
+              </DetailsContext>
+
+            </Switch>
+          </Layout>
+        </VideoContex>
+      </PopularMovieContex> 
+      
+    </Router>
   );
 }
 
-export default App;
+export default App
+
+
