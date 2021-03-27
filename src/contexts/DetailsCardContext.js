5160eb95308149866c1484b121a178c0383bd457
Javier <jeponteg@gmail.com>
2021-03-27 19:31:23 -0300

    Configurado valor por defecto

--- src/contexts/DetailsCardContext.js
+++ src/contexts/DetailsCardContext.js
@@ -1,32 +1,24 @@
 import React,{ useState, useEffect, createContext } from 'react'
-import { getMovieById, getSimilarMovies } from '../api/movie-api';
+import { getSimilarMovies } from '../api/movie-api';
 
 export const DetailsContext = createContext();
 
 const DetailsCardContext = ({children}) => {
 
-    const [movie, setMovie] = useState()
     const [similar, setsimilar] = useState()
     const [id, setId] = useState(null)
 
     useEffect(() => {
-
         if(id !== null){
-            getMovieById(id).then((response) => {
-                setMovie(response);
-            });
             getSimilarMovies(id).then((response) => {
                 setsimilar(response.results);
             });
         }
-        
     }, [id])
   
     return (
         <DetailsContext.Provider
             value={{
-                movie,
-                setMovie,
                 id,
                 setId,
                 similar,
