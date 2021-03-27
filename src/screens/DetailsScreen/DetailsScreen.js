d6ee3c371697e0fc7bf8c93c0013371b73640cc6
Javier <jeponteg@gmail.com>
2021-03-27 19:27:47 -0300

    Configuracion del DetailsScreen

--- src/screens/DetailsScreen/DetailsScreen.js
+++ src/screens/DetailsScreen/DetailsScreen.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import DetailsCard from '../../components/Details-card/DetailsCard'
+import SimilarMovies from '../../components/Movies/general-movies/SimilarMovies'
+import './DetailsScreen.styles.scss'
 
 const DetailsScreen = () => {
     return (
-        <div>
+        <div className="details-screen">
             <DetailsCard/>
-        
+            <SimilarMovies/>
         </div>
     )
 }
