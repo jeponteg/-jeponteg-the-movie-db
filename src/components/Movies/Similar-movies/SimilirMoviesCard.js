775f295fe06cfbfce837b2c7b234849599d78fa2
Javier <jeponteg@gmail.com>
2021-04-10 19:37:15 -0400

    configurado propsType

--- src/components/Movies/Similar-movies/SimilirMoviesCard.js
+++ src/components/Movies/Similar-movies/SimilirMoviesCard.js
@@ -4,8 +4,9 @@ import { BASE_PATH_IMG } from '../../../utils/constants'
 import Card from '@material-ui/core/Card'
 import CardActionArea from '@material-ui/core/CardActionArea'
 import CardMedia from '@material-ui/core/CardMedia'
+import PropTypes from 'prop-types';
 
-const SimilirMoviesCard = ({poster_path}) => {
+const SimilirMoviesCard = ({ idMovie, poster_path }) => {
    
     const{setId} = useContext(VideoContex)
     const imageUrl =`${BASE_PATH_IMG}/w400${poster_path ? poster_path : null}`
@@ -16,7 +17,7 @@ const SimilirMoviesCard = ({poster_path}) => {
 
     return (
         <Card className="card-root">
-            <CardActionArea onClick ={()=>alert('hola mundo')} >
+            <CardActionArea onClick ={()=>handdleOnClick(idMovie)} >
                 <CardMedia
                     className="card-media"
                     image={imageUrl}
@@ -27,3 +28,7 @@ const SimilirMoviesCard = ({poster_path}) => {
     )
 }
 export default SimilirMoviesCard
+
+SimilirMoviesCard.propTypes = {
+    poster_path: PropTypes.string
+}
