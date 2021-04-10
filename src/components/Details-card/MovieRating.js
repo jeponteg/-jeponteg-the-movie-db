76c3d7b163ca11bbe6b35b75c6296bf4cd30b4f2
Javier <jeponteg@gmail.com>
2021-04-10 19:37:53 -0400

    configurado propsType

--- src/components/Details-card/MovieRating.js
+++ src/components/Details-card/MovieRating.js
@@ -3,7 +3,7 @@ import Rating from '@material-ui/lab/Rating';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 
-const MovieRating = ({voteAverage = 0, voteCount = 0}) => {
+const MovieRating = ({voteAverage , voteCount }) => {
   return(
     <Box component="fieldset" mb={3} borderColor="transparent">
       <Typography style={{color:'white'}}component="legend">Votos: {voteCount}</Typography>
@@ -18,3 +18,9 @@ const MovieRating = ({voteAverage = 0, voteCount = 0}) => {
 }
 
 export default MovieRating
+
+MovieRating.defaultProps = {
+  voteAverage: 0,
+  voteCount:0
+};
+
