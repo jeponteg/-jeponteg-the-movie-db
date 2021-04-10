49f9dc0a9ed84d50c78d26997f9defe4590119a3
Javier <jeponteg@gmail.com>
2021-04-10 19:35:52 -0400

    configurado propsType

--- src/components/layouts/Layout.js
+++ src/components/layouts/Layout.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import PropTypes from 'prop-types';
 import '../../assets/styles/components/Layout.scss';
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
     return (
         <div className="layout">
             <div className="content-wrap">
@@ -16,3 +17,7 @@ const Layout = ({children}) => {
 }
 
 export default Layout
+
+Layout.propTypes = {
+    children: PropTypes.element.isRequired
+};
