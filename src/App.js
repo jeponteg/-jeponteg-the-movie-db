e63a4b5596acc3ae91fe36cf4d54d691a7781bed
Javier <jeponteg@gmail.com>
2021-04-10 13:40:27 -0400

    configurado LazyLoading

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React,{ Suspense, lazy } from "react";
 import PopularMovieContex from "./contexts/PopularMovieContext";
 import VideoContex from "./contexts/VideoContext";
 import DetailsContext from "./contexts/DetailsCardContext";
 import Layout from "./components/layouts/Layout";
 import HomeScreen from "./screens/HomeScreen/HomeScreen";
-import { BrowserRouter as Router, Switch, Route, Redirect,useLocation } from "react-router-dom";
 import PageNotFound from "./components/Movies/NotFound/PageNotFound";
-import DetailsScreen from "./screens/DetailsScreen/DetailsScreen";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+const DetailsScreen =lazy(() => import ('./screens/DetailsScreen/DetailsScreen'));
 
 const App = () => {
 
@@ -17,13 +17,17 @@ const App = () => {
           <DetailsContext>
             <Layout>
               <Switch>
-                <Route exact path='/'>
-                  <HomeScreen/>
-                </Route>
-                <Route exact path="/movie-details/:id">
-                  <DetailsScreen/>
-                </Route> 
-                <Route component={PageNotFound} />
+                <Suspense fallback={
+                    <h1>Loading</h1>
+                  }>
+                  <Route exact path='/'>
+                    <HomeScreen/>
+                  </Route> 
+                  <Route exact path="/movie-details/:id">
+                    <DetailsScreen/>
+                  </Route>
+                </Suspense>   
+                <Route path=""component={PageNotFound} />
               </Switch>
             </Layout>
           </DetailsContext>
