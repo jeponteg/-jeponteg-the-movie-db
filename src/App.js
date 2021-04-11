ee763fcff7962a2917012d079b25e4aeac6a3d71
Javier <jeponteg@gmail.com>
2021-04-11 16:05:36 -0400

    ErrorBoundary configurado

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Layout from "./components/layouts/Layout";
 import HomeScreen from "./screens/HomeScreen/HomeScreen";
 import PageNotFound from "./components/Movies/NotFound/PageNotFound";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { ErrorBoundary } from "react-error-boundary";
 const DetailsScreen =lazy(() => import ('./screens/DetailsScreen/DetailsScreen'));
 
 const App = () => {
@@ -16,26 +17,33 @@ const App = () => {
         <VideoContex>
           <DetailsContext>
             <Layout>
-
-                <Suspense fallback={
-                  <h1>Loading</h1>
-                }>
+              <Suspense fallback={ <h1>Loading</h1> }>
+                <ErrorBoundary
+                  fallbackRender={({error, componentStack, resetErrorBoundary}) => (
+                    <div role="alert">
+                      <div>Something went wrong!!</div>
+                      <pre>{error.message}</pre>
+                      <button onClick={() => { resetErrorBoundary() }}>
+                        Try again
+                      </button>
+                    </div>
+                  )}
+                >
                   <Switch>
-                    
-                      <Route exact path="/movie-details/:id">
-                        <DetailsScreen/>
-                      </Route>
-                      <Route exact path='/'>
-                        <HomeScreen/>
-                      </Route> 
-                      <Route path=""component={PageNotFound} />
-                    
+                    <Route exact path="/movie-details/:id">
+                      <DetailsScreen/>
+                    </Route>
+                    <Route exact path="/">
+                      <HomeScreen/>
+                    </Route>  
+                    <Route path="*" component={PageNotFound} />
                   </Switch>
-              </Suspense>
+                </ErrorBoundary>
+              </Suspense>  
             </Layout>
           </DetailsContext>
         </VideoContex>
-    </PopularMovieContex> 
+      </PopularMovieContex> 
     </Router>
   );
 }
